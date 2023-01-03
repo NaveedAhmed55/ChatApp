fe8189b8e4625506e5b4751127afffb78a4d1944
Naveed Ahmed <76599103+NaveedAhmed55@users.noreply.github.com>
2023-01-03 15:15:01 +0500

    Others tasks

---
+++ Link download/app/src/main/java/com/example/httpcon/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.httpcon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Thread(Runnable{
+
+            val url_string="https://tutorialspoint.com/json/data.json"
+            val url= URL(url_string)
+
+        })
+        val queue=Volley.newRequestQueue(this)
+        val strReq=StringRequest(
+            Request.Method.GET,
+            "https://tutorialspoint.com/json/data.json",
+            Response.Listener {
+                findViewById<TextView>(R.id.lblString).text=it
+            },
+            Response.ErrorListener {
+                findViewById<TextView>(R.id.lblString).text=it.message
+            }
+        )
+
+        queue.add(strReq)
+
+    }
+}
